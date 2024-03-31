2babce254794217aa0739c9a3ffec55c8918d257
muhammadalighouri <98492348+muhammadalighouri@users.noreply.github.com>
2024-03-31 07:02:50 +0500

    weew

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ app.delete('/courses/:courseId/topics/:topicId', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
